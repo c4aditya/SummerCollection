658adc74a31036ece60b6a07a45ee6f0107bff9d
Aditya Singh <singhas1418@gmail.com>
2025-06-11 13:22:34 +0530

    implemented Nav bar for all screen size

--- summerCollection/src/Components/NavBar.jsx
+++ summerCollection/src/Components/NavBar.jsx
@@ -3,52 +3,134 @@ import { GoPerson } from "react-icons/go";
 import { CiHeart } from "react-icons/ci";
 import { CiShoppingCart } from "react-icons/ci";
 import { useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { RiArrowDropDownLine } from "react-icons/ri";
 
-function NavBar(){
-    const [menu , setMenu] = useState(false)
-    function handleMenu(){
+function NavBar() {
+    const [menu, setMenu] = useState(false)
+    function handleMenu() {
         console.log("The Button is clicked")
         setMenu(!menu)
     }
-    return(
+
+    function handleClose() {
+        setMenu(!menu)
+    }
+    if (menu) {
+        document.body.style.backgroundColor = "#999999"
+    }
+    else {
+        document.body.style.backgroundColor = "white"
+    }
+    return (
         <>
             <section className="nav-section">
-                   
-                    <div className="nav-bar ">
-                        <div className="letf-nav-bar">
+
+                <div className="nav-bar ">
+                    <div className="letf-nav-bar">
+                        <div className="hamburger">
+                            <ul>
+                                <li onClick={handleMenu}><RxHamburgerMenu /></li>
+                            </ul>
+                        </div>
+                        <div className="catogries">
+                            <ul>
+
+                                <li>Men</li>
+                                <li>Women</li>
+                                <li>Juinor</li>
+                            </ul>
+                        </div>
+
+
+                    </div>
+                    <div className="logo">
+                        <p>Summer Collections</p>
+                    </div>
+
+                    <div className="right-nav-bar">
 
                         <ul>
-                            <li onClick={handleMenu}><RxHamburgerMenu/></li>
-                            <li>Men</li>
-                            <li>Women</li>
-                            <li>Juinor</li>
+                            <li><GoPerson /> </li>
+                            <li><CiHeart /></li>
+                            <li><CiShoppingCart /> </li>
                         </ul>
-
-                        </div>
-                        <div className="logo">
-                            <p>Summer Collections</p>
-                        </div>
-
-                        <div className="right-nav-bar">
-
-                         <ul>
-                            <li><GoPerson/></li>
-                            <li><CiHeart/></li>
-                            <li><CiShoppingCart/></li>
-                         </ul>
-
-                        </div>
 
                     </div>
 
+                </div>
 
-               
+
+
             </section>
 
             <aside>
-                <div className="sideMenubar">
+                <div className={`sideMenubar ${menu ? "open" : " "}`}>
+
                     {
-                       menu ?(<div> <h1>Meanus is clicked</h1></div>) :(<p> </p>)
+                        menu ? (<div>
+                            <div className="side-bar-nav-bar">
+                                <div className="cross ">
+                                    <button onClick={handleClose}><IoMdClose /></button>
+                                </div>
+                                <div className="top-div-class-logo-and-buttons">
+                                    <div className="logo">
+                                        <h1>logo</h1>
+                                    </div>
+
+                                    <div className="top-side-bar-buttom">
+                                        <ul>
+                                            <li><span><GoPerson /> </span> <button className="btn">Login / Register</button>  </li>
+                                        </ul>
+
+                                    </div>
+                                </div>
+
+                                <div className="productLists">
+                                    <table>
+                                        <tr>
+                                            <th>Tranding</th>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Bestseller <span><RiArrowDropDownLine /></span></td>
+                                            <td>New Releases <span><RiArrowDropDownLine /></span></td>
+                                        </tr>
+                                    </table>
+                                </div>
+
+                                <div className="productLists">
+                                    <table>
+                                        <tr>
+                                            <th>Categories</th>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Men <span><RiArrowDropDownLine /></span></td>
+                                            <td>Women <span><RiArrowDropDownLine /></span></td>
+                                            <td>Kids <span><RiArrowDropDownLine /></span></td>
+                                        </tr>
+                                    </table>
+                                </div>
+
+                                <div className="productLists">
+                                    <table>
+                                        <tr>
+                                            <th>Help & Setting</th>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Your Account <span><RiArrowDropDownLine /></span></td>
+                                            <td>Customer Service  <span><RiArrowDropDownLine /></span></td>
+                                            <td>Sign in<span><RiArrowDropDownLine /></span></td>
+                                        </tr>
+                                    </table>
+                                </div>
+
+
+
+                            </div>
+                        </div>) : (<p> </p>)
                     }
                 </div>
             </aside>
